eff99467d494977ed84a8fc4c6bb165d429b9593
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-12-29 11:44:10 +0100

    Create vpc module

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -1,58 +1,58 @@
 #Create AWS VPC
 resource "aws_vpc" "demo-vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = var.VPC_CIDR_BLOCK
   instance_tenancy = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
 
   tags = {
-    Name = "demo-vpc"
+    Name = "${var.VPC_NAME}"
   }
 }
 
 # Public Subnets in Custom VPC
 resource "aws_subnet" "public-subnet-A" {
   vpc_id                  = aws_vpc.demo-vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = var.SUBNET_IPS[0]
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-1a"
+  availability_zone       = var.AVAILABILITY_ZONES[0]
 
   tags = {
-    Name = "public-subnet-A"
+    Name = "${var.SUBNET_NAMES[0]}"
   }
 }
 
 resource "aws_subnet" "public-subnet-B" {
   vpc_id                  = aws_vpc.demo-vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = var.SUBNET_IPS[1]
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-1b"
+  availability_zone       = var.AVAILABILITY_ZONES[1]
 
   tags = {
-    Name = "public-subnet-B"
+    Name = "${var.SUBNET_NAMES[1]}"
   }
 }
 
 # Private Subnets in Custom VPC
 resource "aws_subnet" "private-subnet-A" {
   vpc_id                  = aws_vpc.demo-vpc.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = var.SUBNET_IPS[2]
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-1a"
+  availability_zone       = var.AVAILABILITY_ZONES[0]
 
   tags = {
-    Name = "private-subnet-A"
+    Name = "${var.SUBNET_NAMES[2]}"
   }
 }
 
 resource "aws_subnet" "private-subnet-B" {
   vpc_id                  = aws_vpc.demo-vpc.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block              = var.SUBNET_IPS[3]
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-1b"
+  availability_zone       = var.AVAILABILITY_ZONES[1]
 
   tags = {
-    Name = "private-subnet-B"
+    Name = "${var.SUBNET_NAMES[3]}"
   }
 }
 
@@ -61,7 +61,7 @@ resource "aws_internet_gateway" "demo-gw" {
   vpc_id = aws_vpc.demo-vpc.id
 
   tags = {
-    Name = "demo-gw"
+    Name = "${var.GW_NAME}"
   }
 }
 
@@ -74,7 +74,7 @@ resource "aws_route_table" "demo-rt" {
   }
 
   tags = {
-    Name = "demo-rt"
+    Name = "${var.PUBLIC_RT_NAME}"
   }
 }
 
@@ -87,3 +87,24 @@ resource "aws_route_table_association" "public-B" {
   subnet_id      = aws_subnet.public-subnet-B.id
   route_table_id = aws_route_table.demo-rt.id
 }
+
+#Module Outputs
+output "vpc_id" {
+  value = aws_vpc.demo-vpc.id
+} 
+
+output "internet_gateway" {
+  value = aws_internet_gateway.demo-gw
+} 
+
+output "public_subnet_A_id" {
+  value = aws_subnet.public-subnet-A.id
+} 
+
+output "private_subnet_id" {
+  value = aws_subnet.private-subnet-B.id
+} 
+
+output "public_subnet_B_id" {
+  value = aws_subnet.public-subnet-B.id
+} 
